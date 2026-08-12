d5699a914e0dd02ffc3f5d8dc2b8a1908bf44af6
shubhamkurade21 <kuradeshubham21@gmail.com>
2026-08-12 12:45:16 +0530

    Exception ResourceNotFoundException added

---
+++ Server/src/main/java/com/backend/common/exception/ResourceNotFoundException.java
@@ -0,0 +1,14 @@
+package com.backend.common.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(String resource, Long id) {
+		super(resource + id);
+	}
+
+}
